b52c999f047998ef8664885d2eb8bfc062f7eb2c
Maxim Samsonov <m.samsonov@computer.org>
2026-02-13 22:30:36 +0300

    wip 3

--- src/main_demo.cpp
+++ src/main_demo.cpp
@@ -55,7 +55,10 @@ int main(int argc, char** argv) {
             Ili9488 lcd(spi, dcLine, rstLine, width, height);
             lcd.reset();
             lcd.init();
+            lcd.fill(0xF800);  // Red screen - should appear immediately
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000));  // Wait 2 sec to see it
             lcd.fill(0x0000);
+
 
             FourLineDisplay display(width, height, small_font, large_font);
             if (!display.initialize(font)) {
